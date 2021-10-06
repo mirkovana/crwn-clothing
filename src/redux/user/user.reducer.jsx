adfa0367184b4c6f2a34f67c2aea55bdc85792c6
mana81098@gmail.com <mana81098@gmail.com>
2021-10-06 15:47:45 +0200

    Updating app to redirect to home if user is signed in

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,3 +1,5 @@
+import { UserActionTypes } from "./user.types";
+
 const INITIAL_STATE = {
   currentUser: null,
 };
@@ -5,7 +7,7 @@ const INITIAL_STATE = {
 const userReducer = (state = INITIAL_STATE, action) => {
   //state = INITIAL_STATE oznacava da ako state ne bude nista onda ce se postaviti na defaultnu vrednost koja je definisana u initial_state, a ako ima neku vrednost onda ce biti ta vrednost koja jeste
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
