1af6e44dc8ad76b6f9d9f5f2cb8684f73945047c
mana81098@gmail.com <mana81098@gmail.com>
2021-10-20 17:25:06 +0200

    Directory state into redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 //Moze biti kao sto je napisano u liniji ispod ali ako budemo prosirivali listu middleware-a onda nije prakticno
 //const store = createStore(rootReducer, applyMiddleware(logger));
-export default store;
+
+export const persistor = persistStore(store);
+
+export default { store, persistor };
