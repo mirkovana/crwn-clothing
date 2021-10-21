45ab90c2b4fe098e42caec3ff3ddf9e1e87bba07
mana81098@gmail.com <mana81098@gmail.com>
2021-10-21 14:54:54 +0200

    New products flow for directory/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ collections }) => (
+const ShopPage = (
+  { match } //posto je u App.js-u kad se poziva ShopPage vec stavljen Route onda cemo ovde kao props primiti tri stvari, a to su match,location, history
+) => (
   <div className="shop-page">
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,3 +6,14 @@ export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key]) //vraca listu kljuceva u obliku stringa od objekta koji mu je prosledjen
+);
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  );
