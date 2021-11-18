f15300ca65e91881986f4d19a6290d617518015d
mana81098@gmail.com <mana81098@gmail.com>
2021-11-18 18:05:19 +0100

    Fix

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -154,7 +154,7 @@ const SHOP_DATA = {
   },
   womens: {
     id: 4,
-    title: "Womens",
+    title: "Women",
     routeName: "womens",
     items: [
       {
@@ -203,7 +203,7 @@ const SHOP_DATA = {
   },
   mens: {
     id: 5,
-    title: "Mens",
+    title: "Men",
     routeName: "mens",
     items: [
       {
